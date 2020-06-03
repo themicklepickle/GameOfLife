14f16ef616c9c1ae67daec6ff372ca084d00b18b
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2020-06-03 16:21:59 -0600

    Cleaned up code

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@
 
 
 # import pygame module for graphical user interface
-import pygame 
+import pygame
 
 # pygame setup
 pygame.init()
@@ -17,7 +17,7 @@ screen_width, screen_height = pygame.display.get_surface().get_size()
 
 
 def alive(grid, coordinate):
-	"""
+    """
     Determines whether a cell is alive or not.
 
     Args:
@@ -33,45 +33,45 @@ def alive(grid, coordinate):
         >>> alive([[0, 1, 1], [0, 0, 1], [1, 0, 1]], (1, 1))
         False
     """
-	
-	# make a copy of grid parameter to not change original grid
-	temp_grid = grid.copy()
-	
-	# process coordinate
-	x, y = coordinate[0] + 1, coordinate[1] + 1
 
-	# add buffer around matrix to avoid IndexError when referencing cells around the edges
-	temp_grid.insert(0, [0] * len(temp_grid[0]))
-	temp_grid.append([0] * len(temp_grid[0]))
-	for i in range(len(temp_grid)):
-		row_ = temp_grid[i].copy()
-		row_.insert(0, 0)
-		row_.append(0)
-		temp_grid[i] = row_
+    # make a copy of grid parameter to not change original grid
+    temp_grid = grid.copy()
 
-	# count number of alive neighbours
-	cell = temp_grid[y][x]
-	count = 0
-	for i in range(-1, 2):
-		for j in range(-1, 2):
-			if i == 0 and j == 0:
-				continue
-			else:
-				if temp_grid[y + i][x + j] == 1:
-					count += 1
+    # process coordinate
+    x, y = coordinate[0] + 1, coordinate[1] + 1
 
-	# return alive or dead according to conway's game of life rules
-	if cell == 0:
-		if count == 3:
-			return True
-	else:
-		if count == 2 or count == 3:
-			return True
-	return False
+    # add buffer around matrix to avoid IndexError when referencing cells around the edges
+    temp_grid.insert(0, [0] * len(temp_grid[0]))
+    temp_grid.append([0] * len(temp_grid[0]))
+    for i in range(len(temp_grid)):
+        row_ = temp_grid[i].copy()
+        row_.insert(0, 0)
+        row_.append(0)
+        temp_grid[i] = row_
+
+    # count number of alive neighbours
+    cell = temp_grid[y][x]
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            else:
+                if temp_grid[y + i][x + j] == 1:
+                    count += 1
+
+    # return alive or dead according to conway's game of life rules
+    if cell == 0:
+        if count == 3:
+            return True
+    else:
+        if count == 2 or count == 3:
+            return True
+    return False
 
 
 def input_grid(matrix_width, matrix_height):
-	"""
+    """
     Uses pygame to input a user-created grid.
 
     Args:
@@ -85,74 +85,74 @@ def input_grid(matrix_width, matrix_height):
         >>> input_grid(3, 3)
         [[0, 1, 1], [0, 0, 1], [1, 0, 1]]
     """
-	
-	# clear screen
-	pygame.draw.rect(win, (230, 230, 230), (0, 0, screen_width, screen_height))
-	
-	# create a temporary grid containing 2's in order to draw an empty grid
-	temp_grid = [[2 for j in range(matrix_width)] for i in range(matrix_height)]
-	
-	# calculate grid height, cell size, and grid width with arguments
-	if matrix_height >= matrix_width:
-		grid_height = screen_height - 200
-		cell_size = int(grid_height / matrix_height)
-		grid_width = cell_size * matrix_width
-	else:
-		grid_width = screen_width - 200
-		cell_size = int(grid_width / matrix_width)
-		grid_height = cell_size * matrix_height
 
-	# display instructions below grid
-	arial_40 = pygame.font.SysFont("Arial", 40)
-	text = arial_40.render(f"The Yellow cell is currently selected. Press 1 for alive or 0 for dead.", True, (0, 0, 0))
-	win.blit(text, (screen_width / 2 - 450, screen_height - 60))
+    # clear screen
+    pygame.draw.rect(win, (230, 230, 230), (0, 0, screen_width, screen_height))
 
-	# draw a grid with all cells filled in with white
-	for row in range(len(temp_grid)):
-		for col in range(len(temp_grid[row])):
-			pygame.draw.rect(win, (255, 255, 255), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
-			pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
-	pygame.display.update()
-	
-	# create a new matrix
-	real_grid = []
+    # create a temporary grid containing 2's in order to draw an empty grid
+    temp_grid = [[2 for j in range(matrix_width)] for i in range(matrix_height)]
 
-	# input user choice of alive or dead for each cell in the grid
-	for row in range(len(temp_grid)):
-		real_row = []
-		for col in range(len(temp_grid[row])):
-			
-			# draw selected cell filled in with yellow 
-			pygame.draw.rect(win, (245, 209, 66), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
-			pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
-			pygame.display.update()
-			
-			# receive user input
-			while True:
-				pygame.event.get()
-				pygame.time.delay(100)
-				keys = pygame.key.get_pressed()
-				
-				# redraw selected cell filled in with green if the cell is alive
-				if keys[pygame.K_1]:
-					real_row.append(1)
-					pygame.draw.rect(win, (63, 235, 63), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
-					break
+    # calculate grid height, cell size, and grid width with arguments
+    if matrix_height >= matrix_width:
+        grid_height = screen_height - 200
+        cell_size = int(grid_height / matrix_height)
+        grid_width = cell_size * matrix_width
+    else:
+        grid_width = screen_width - 200
+        cell_size = int(grid_width / matrix_width)
+        grid_height = cell_size * matrix_height
 
-				# redraw selected cell filled in with red if the cell is dead
-				if keys[pygame.K_0]:
-					real_row.append(0)
-					pygame.draw.rect(win, (255, 54, 54), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
-					break
-			
-			# draw border around cell
-			pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
-		real_grid.append(real_row)
-	return real_grid
+    # display instructions below grid
+    arial_40 = pygame.font.SysFont("Arial", 40)
+    text = arial_40.render(f"The Yellow cell is currently selected. Press 1 for alive or 0 for dead.", True, (0, 0, 0))
+    win.blit(text, (screen_width / 2 - 450, screen_height - 60))
+
+    # draw a grid with all cells filled in with white
+    for row in range(len(temp_grid)):
+        for col in range(len(temp_grid[row])):
+            pygame.draw.rect(win, (255, 255, 255), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
+            pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+    pygame.display.update()
+
+    # create a new matrix
+    real_grid = []
+
+    # input user choice of alive or dead for each cell in the grid
+    for row in range(len(temp_grid)):
+        real_row = []
+        for col in range(len(temp_grid[row])):
+
+            # draw selected cell filled in with yellow
+            pygame.draw.rect(win, (245, 209, 66), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
+            pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+            pygame.display.update()
+
+            # receive user input
+            while True:
+                pygame.event.get()
+                pygame.time.delay(100)
+                keys = pygame.key.get_pressed()
+
+                # redraw selected cell filled in with green if the cell is alive
+                if keys[pygame.K_1]:
+                    real_row.append(1)
+                    pygame.draw.rect(win, (63, 235, 63), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
+                    break
+
+                # redraw selected cell filled in with red if the cell is dead
+                if keys[pygame.K_0]:
+                    real_row.append(0)
+                    pygame.draw.rect(win, (255, 54, 54), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
+                    break
+
+            # draw border around cell
+            pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+        real_grid.append(real_row)
+    return real_grid
 
 
 def draw_grid(grid, generation, mode):
-	"""
+    """
     Uses pygame to display a grid.
 
     Args:
@@ -166,73 +166,73 @@ def draw_grid(grid, generation, mode):
     Example:
         >>> draw_grid([[0, 1, 1], [0, 0, 1], [1, 0, 1]], 5, 1)
     """
-	
-	# clear screen
-	pygame.draw.rect(win, (230, 230, 230), (0, 0, screen_width, screen_height))
-	
-	# calculate matrix height and width
-	matrix_height = len(grid)
-	matrix_width = len(grid[0])
 
-	# calculate grid height, cell size, and grid width
-	if matrix_height >= matrix_width:
-		grid_height = screen_height - 200
-		cell_size = int(grid_height / matrix_height)
-		grid_width = cell_size * matrix_width
-	else:
-		grid_width = screen_width - 200
-		cell_size = int(grid_width / matrix_width)
-		grid_height = cell_size * matrix_height
-		
-	# generate font with pygame
-	arial_40 = pygame.font.SysFont("Arial", 40)
+    # clear screen
+    pygame.draw.rect(win, (230, 230, 230), (0, 0, screen_width, screen_height))
 
-	# display instructions below grid if method of proceeding to next generation is manual
-	if mode == 1:
-		text = arial_40.render(f"Press ENTER to proceed to the next generation", True, (0, 0, 0))
-		win.blit(text, (screen_width / 2 - 315, screen_height - 60))
+    # calculate matrix height and width
+    matrix_height = len(grid)
+    matrix_width = len(grid[0])
 
-	# display generation number above grid
-	text = arial_40.render(f"Generation: {generation}", True, (0, 0, 0))
-	win.blit(text, (screen_width / 2 - 110, 40))
+    # calculate grid height, cell size, and grid width
+    if matrix_height >= matrix_width:
+        grid_height = screen_height - 200
+        cell_size = int(grid_height / matrix_height)
+        grid_width = cell_size * matrix_width
+    else:
+        grid_width = screen_width - 200
+        cell_size = int(grid_width / matrix_width)
+        grid_height = cell_size * matrix_height
 
-	# draw each cell in the grid
-	for row in range(len(grid)):
-		for col in range(len(grid[row])):
+    # generate font with pygame
+    arial_40 = pygame.font.SysFont("Arial", 40)
 
-			# draw cell filled in with green if cell is a 1 (alive)
-			if grid[row][col] == 1:
-				pygame.draw.rect(win, (63, 235, 63), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
-				pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+    # display instructions below grid if method of proceeding to next generation is manual
+    if mode == 1:
+        text = arial_40.render(f"Press ENTER to proceed to the next generation", True, (0, 0, 0))
+        win.blit(text, (screen_width / 2 - 315, screen_height - 60))
 
-			# draw cell filled in with red if cell is a 0 (dead)
-			else:
-				pygame.draw.rect(win, (255, 54, 54), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
-				pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+    # display generation number above grid
+    text = arial_40.render(f"Generation: {generation}", True, (0, 0, 0))
+    win.blit(text, (screen_width / 2 - 110, 40))
 
-	# update pygame display
-	pygame.display.update()
-	
-	
+    # draw each cell in the grid
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+
+            # draw cell filled in with green if cell is a 1 (alive)
+            if grid[row][col] == 1:
+                pygame.draw.rect(win, (63, 235, 63), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
+                pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+
+            # draw cell filled in with red if cell is a 0 (dead)
+            else:
+                pygame.draw.rect(win, (255, 54, 54), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size))
+                pygame.draw.rect(win, (0, 0, 0), (int((screen_width - grid_width) / 2 + cell_size * col), int((screen_height - grid_height) / 2 + cell_size * row), cell_size, cell_size), 1)
+
+    # update pygame display
+    pygame.display.update()
+
+
 def main_screen():
-	"""
-	Uses pygame to display main screen with title, creator, and instructions.
-	"""
+    """
+    Uses pygame to display main screen with title, creator, and instructions.
+    """
 
-	# display game title
-	arial_80 = pygame.font.SysFont("Arial", 80)
-	text = arial_80.render("Conway's Game of Life!", True, (0, 0, 0), (230, 230, 230))
-	win.blit(text, (screen_width / 2 - 320, 200))
+    # display game title
+    arial_80 = pygame.font.SysFont("Arial", 80)
+    text = arial_80.render("Conway's Game of Life!", True, (0, 0, 0), (230, 230, 230))
+    win.blit(text, (screen_width / 2 - 320, 200))
 
-	# display creator
-	arial_40 = pygame.font.SysFont("Arial", 40)
-	text = arial_40.render("By Michael Xu", True, (0, 0, 0), (230, 230, 230))
-	win.blit(text, (screen_width / 2 - 110, 300))
+    # display creator
+    arial_40 = pygame.font.SysFont("Arial", 40)
+    text = arial_40.render("By Michael Xu", True, (0, 0, 0), (230, 230, 230))
+    win.blit(text, (screen_width / 2 - 110, 300))
 
-	# display instructions
-	arial_40 = pygame.font.SysFont("Arial", 40)
-	text = arial_40.render("Please return to terminal", True, (255, 54, 54), (230, 230, 230))
-	win.blit(text, (screen_width / 2 - 175, 500))
+    # display instructions
+    arial_40 = pygame.font.SysFont("Arial", 40)
+    text = arial_40.render("Please return to terminal", True, (255, 54, 54), (230, 230, 230))
+    win.blit(text, (screen_width / 2 - 175, 500))
 
-	# update pygame display
-	pygame.display.update()
+    # update pygame display
+    pygame.display.update()
--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 # May 19, 2020
 
 
-# import all supporting modules
-import pygame
 import ast
 from datetime import date, datetime
-from functions import *
+
+# import all supporting modules
+import pygame
 import sanitization as s
+from functions import *
 
 # display title screen
 main_screen()
@@ -18,74 +19,74 @@ main_screen()
 print("INPUT PARAMETERS:")
 grid_type = s.integer("Existing grid (0) or New grid (1): ", 0, 1)
 if grid_type == 1:
-	width = s.integer("Grid width: ", None, None)
-	height = s.integer("Grid height: ", None, None)
+    width = s.integer("Grid width: ", None, None)
+    height = s.integer("Grid height: ", None, None)
 gens = s.integer("Generations: ", 1, None)
 mode = s.integer("Automatic (0) or Manual (1) Generations: ", 0, 1)
 if mode == 0:
-	delay = s.floating_point("Time Delay (secs): ", 0, None)
+    delay = s.floating_point("Time Delay (secs): ", 0, None)
 if grid_type == 1:
-	print("DONE. Please return to game window")
-	grid = input_grid(width, height)
+    print("DONE. Please return to game window")
+    grid = input_grid(width, height)
 else:
-	grid = ast.literal_eval(input("Existing grid (one line matrix): "))
-	print("DONE. Please return to game window.")
+    grid = ast.literal_eval(input("Existing grid (one line matrix): "))
+    print("DONE. Please return to game window.")
 
 # write grid to history file for future reference
 history = open("history.txt", "a")
 history.write(f"date: {date.today().strftime('%m/%d/%y')}, time: {datetime.now().strftime('%H:%M:%S')}\n")
-history.write("Copy: "+str(grid)+"\n")
+history.write("Copy: " + str(grid) + "\n")
 history.write("[")
 for row in range(len(grid)):
-	if row == 0:
-		history.write("[")
-	else:
-		history.write(" [")
-	for col in range(len(grid[row])):
-		if col == len(grid[row]) - 1:
-			history.write(str(grid[row][col]))
-		else:
-			history.write(str(grid[row][col]) + ", ")
-	if row == len(grid) - 1:
-		history.write("]")
-	else:
-		history.write("],\n")
+    if row == 0:
+        history.write("[")
+    else:
+        history.write(" [")
+    for col in range(len(grid[row])):
+        if col == len(grid[row]) - 1:
+            history.write(str(grid[row][col]))
+        else:
+            history.write(str(grid[row][col]) + ", ")
+    if row == len(grid) - 1:
+        history.write("]")
+    else:
+        history.write("],\n")
 history.write("]\n\n")
 history.close()
 
 # main program
 for generation in range(gens + 1):
 
-	# draw grid in game window
-	draw_grid(grid, generation, mode)
+    # draw grid in game window
+    draw_grid(grid, generation, mode)
 
-	# wait before proceeding to the next generation
-	if mode == 0:
-		pygame.time.delay(int(delay*1000))
-		pygame.event.get()
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_ESCAPE]:
-			pygame.quit()
-	else:
-		while True:
-			pygame.event.get()
-			pygame.time.delay(100)
-			keys = pygame.key.get_pressed()
-			if keys[pygame.K_RETURN]:
-				break
-			if keys[pygame.K_ESCAPE]:
-				pygame.quit()
+    # wait before proceeding to the next generation
+    if mode == 0:
+        pygame.time.delay(int(delay * 1000))
+        pygame.event.get()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            pygame.quit()
+    else:
+        while True:
+            pygame.event.get()
+            pygame.time.delay(100)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_RETURN]:
+                break
+            if keys[pygame.K_ESCAPE]:
+                pygame.quit()
 
-	# update grid
-	old_grid = grid.copy()
-	for row in range(len(old_grid)):
-		old_row = old_grid[row].copy()
-		for col in range(len(old_row)):
-			if alive(old_grid, (col, row)):
-				old_row[col] = 1
-			else:
-				old_row[col] = 0
-		grid[row] = old_row
+    # update grid
+    old_grid = grid.copy()
+    for row in range(len(old_grid)):
+        old_row = old_grid[row].copy()
+        for col in range(len(old_row)):
+            if alive(old_grid, (col, row)):
+                old_row[col] = 1
+            else:
+                old_row[col] = 0
+        grid[row] = old_row
 
 # close pygame window
 pygame.quit()
